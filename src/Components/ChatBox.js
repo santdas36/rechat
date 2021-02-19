21f8f1286201550394ac35edc6cb86a0af77e231
santdas36 <santdas36@gmail.com>
2021-02-20 00:53:13 +0530

    msgs

--- src/Components/ChatBox.js
+++ src/Components/ChatBox.js
@@ -150,7 +150,7 @@ function ChatBox() {
 		<form onSubmit={sendMessage} className="chat__input">
 			<input onChange={addImage} type="file" ref={fileElem} accept="image/*" style={{display:'none'}} />
 			<button type="button" onClick={(e)=> {e.preventDefault(); fileElem.current.click();}} className={`addImg ${fileSelected ? 'green' : ''}`}><svg fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"></path></svg></button>
-			<input value={userInp} onChange={(e)=>setUserInp(e.target.value)} type="text" required placeholder="Type Here..."/>
+			<input value={userInp} onChange={(e)=>setUserInp(e.target.value)} type="text" required={!fileSelected} placeholder="Type Here..."/>
 			<button type="submit" disabled={loading} className="submit">{loading? '...' : 'Send'}</button>
 		</form>
 		</>)
