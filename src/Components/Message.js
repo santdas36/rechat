e1badaecec15400a858990a995ada97800b624b3
santdas36 <santdas36@gmail.com>
2021-02-20 01:36:05 +0530

    likes motion

--- src/Components/Message.js
+++ src/Components/Message.js
@@ -35,17 +35,17 @@ function Message({msg, userDet}) {
 	}
 	
   return (
-    	<div onDoubleClick={addLike} className={`message ${userDet ? 'uudet' : ''} ${msgData.likes.value > 0 ? 'likeAvailable' : ''} ${user.email === msgData.user.email ? 'sent' : ''}`}>
+    	<div onDoubleClick={addLike} className={`message ${userDet ? 'uudet' : ''} ${msgData.likes.length > 0 ? 'likeAvailable' : ''} ${user.email === msgData.user.email ? 'sent' : ''}`}>
 			{userDet && <div className="udet">
 				<img src={msgData.user.photoURL}/>
 				<span>{msgData.user.name.split(' ')[0]}</span>
 			</div>}
 			{msgData.img && <div className="img"><img src={msgData.img.src}/></div>}
 			<p className="text">{msgData.message}</p>
-			{msgData.likes.length > 0 && (<span className="like" onClick={toggleLike}>
+			{msgData.likes.length > 0 && (<motion.span initial={{opacity: 0, scale: 0.8}} animate={{opacity: 1, scale: 1}} transition={{duartion: 0.5, type: 'tween'}} exit={{opacity: 0, scale: 0.8}} className="like" onClick={toggleLike}>
 				<svg fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" clip-rule="evenodd"></path></svg>
 				<span>{msgData.likes.length}</span>
-			</span>)}
+			</motion.span>)}
 		</div>
   );
 }
