2bd2d69574a22db8c70458310283bb3d2269d413
santdas36 <santdas36@gmail.com>
2021-02-20 01:21:33 +0530

    likes

--- src/Components/Message.js
+++ src/Components/Message.js
@@ -14,12 +14,17 @@ function Message({msg, userDet}) {
 	const [{user}, dispatch] = useStateValue();	
 	
 	const addLike = () => {
-		if (!msgData.likes.liked_by.includes(user.email)) {
+		if (!msgData.likes.includes(user.email)) {
 			db.collection('rooms').doc(roomId).collection('messages').doc(msgId).set({
-				likes: {
-					value: firebase.firestore.FieldValue.increment(1),
-					liked_by: firebase.firestore.FieldValue.arrayUnion(user.email),
-				}
+				likes: firebase.firestore.FieldValue.arrayUnion(user.email),
+			}, {merge: true});
+		}
+	}
+	
+	const removeLike = () => {
+		if (msgData.likes.includes(user.email)) {
+			db.collection('rooms').doc(roomId).collection('messages').doc(msgId).set({
+				likes: firebase.firestore.FieldValue.arrayRemove(user.email),
 			}, {merge: true});
 		}
 	}
@@ -32,9 +37,9 @@ function Message({msg, userDet}) {
 			</div>}
 			{msgData.img && <div className="img"><img src={msgData.img.src}/></div>}
 			<p className="text">{msgData.message}</p>
-			{msgData.likes.value > 0 && (<span className="like">
+			{msgData.likes.length > 0 && (<span className="like">
 				<svg fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" clip-rule="evenodd"></path></svg>
-				<span>{msgData.likes.value}</span>
+				<span>{msgData.likes.length}</span>
 			</span>)}
 		</div>
   );
