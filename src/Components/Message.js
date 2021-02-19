2aff6d76ae61a856b2be4ac858e9585af56b1921
santdas36 <santdas36@gmail.com>
2021-02-20 01:25:35 +0530

    toggleLike

--- src/Components/Message.js
+++ src/Components/Message.js
@@ -12,18 +12,23 @@ function Message({msg, userDet}) {
 	const msgId = msg.msgId;
 	const msgData = msg.msgData;
 	const [{user}, dispatch] = useStateValue();	
+	const msgRef = db.collection('rooms').doc(roomId).collection('messages').doc(msgId);
 	
 	const addLike = () => {
 		if (!msgData.likes.includes(user.email)) {
-			db.collection('rooms').doc(roomId).collection('messages').doc(msgId).set({
+			msgRef.set({
 				likes: firebase.firestore.FieldValue.arrayUnion(user.email),
 			}, {merge: true});
 		}
 	}
 	
-	const removeLike = () => {
-		if (msgData.likes.includes(user.email)) {
-			db.collection('rooms').doc(roomId).collection('messages').doc(msgId).set({
+	const toggleLike = () => {
+		if (!msgData.likes.includes(user.email)) {
+			msgRef.set({
+				likes: firebase.firestore.FieldValue.arrayUnion(user.email),
+			}, {merge: true});
+		} else {
+			msgRef.set({
 				likes: firebase.firestore.FieldValue.arrayRemove(user.email),
 			}, {merge: true});
 		}
@@ -37,7 +42,7 @@ function Message({msg, userDet}) {
 			</div>}
 			{msgData.img && <div className="img"><img src={msgData.img.src}/></div>}
 			<p className="text">{msgData.message}</p>
-			{msgData.likes.length > 0 && (<span className="like">
+			{msgData.likes.length > 0 && (<span className="like" onClick={toggleLike}>
 				<svg fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" clip-rule="evenodd"></path></svg>
 				<span>{msgData.likes.length}</span>
 			</span>)}
