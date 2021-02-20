b8e5bb59ec2abcbf54110fe5f2737af20646a029
santdas36 <santdas36@gmail.com>
2021-02-20 15:41:55 +0530

    secs

--- src/Components/Message.js
+++ src/Components/Message.js
@@ -14,7 +14,6 @@ function Message({msg, userDet}) {
 	const msgData = msg.msgData;
 	const [{user}, dispatch] = useStateValue();	
 	const msgRef = db.collection('rooms').doc(roomId).collection('messages').doc(msgId);
-	console.log(msgData.timestamp);
 	
 	const addLike = () => {
 		if (!msgData.likes.includes(user.email)) {
@@ -44,7 +43,7 @@ function Message({msg, userDet}) {
 			</div>}
 			{msgData.img && <div className="img"><img src={msgData.img.src}/></div>}
 			{msgData.message && <p className="text">{msgData.message}</p>}
-			<p className="time">{formatTime('%e %b, %l:%M%P', new Date(msgData.timestamp))}</p>
+			<p className="time">{formatTime('%e %b, %l:%M%P', new Date(msgData.timestamp.seconds))}</p>
 			{msgData.likes.length > 0 && (<motion.span initial={{opacity: 0, scale: 0.8}} animate={{opacity: 1, scale: 1}} transition={{duartion: 0.5, type: 'tween'}} exit={{opacity: 0, scale: 0.8}} className="like" onClick={toggleLike}>
 				<svg fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" clip-rule="evenodd"></path></svg>
 				<span>{msgData.likes.length}</span>
