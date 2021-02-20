3407fab737e74bac17b538ef9fedcdf7faa2c9b0
santdas36 <santdas36@gmail.com>
2021-02-20 18:33:06 +0530

    tSpan

--- src/Components/Message.js
+++ src/Components/Message.js
@@ -44,7 +44,7 @@ function Message({msg, userDet}) {
 			</div>}
 			{msgData.img && <div className="img"><img src={msgData.img.src}/></div>}
 			{msgData.message && <p className="text">{msgData.message}</p>}
-			{msgData.timestamp && <p className="time"><span>{formatTime('%e %b', time)}</span>{formatTime(', %l:%M%P', time)}</p>}
+			{msgData.timestamp && <p className="time">{formatTime('%e %b', time)}<span>{formatTime(', %l:%M%P', time)}</span></p>}
 			{msgData.likes.length > 0 && (<motion.span initial={{opacity: 0, scale: 0.8}} animate={{opacity: 1, scale: 1}} transition={{duartion: 0.5, type: 'tween'}} exit={{opacity: 0, scale: 0.8}} className="like" onClick={toggleLike}>
 				<svg fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" clip-rule="evenodd"></path></svg>
 				<span>{msgData.likes.length}</span>
